a421e62e4de10a90a39b75c771271496034831af
farihatulmaria <farihatulmaria@gmail.com>
2021-08-24 23:39:39 +0600

    First day

---
+++ assets/js/section-init-3.js
@@ -0,0 +1,11 @@
+(function ($) {
+  "use strict";
+
+  $(document).ready(function () {
+    $(".parent-container").magnificPopup({
+      type: "video",
+    });
+  });
+
+  $(Window).load(function () {});
+})(jQuery);
