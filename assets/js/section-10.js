d3c2a25d33bafdeaa1e3b94fbddad3ec6a88bd0a
farihatulmaria <farihatulmaria@gmail.com>
2021-08-27 18:23:29 +0600

    update

---
+++ assets/js/section-10.js
@@ -0,0 +1,11 @@
+(function ($) {
+  "use strict";
+
+  $(".single-item").slick({
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    nav: true,
+  });
+})(jQuery);
