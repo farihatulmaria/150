571cd013f72df5fe85428486674a53e7a9812090
farihatulmaria <farihatulmaria@gmail.com>
2021-09-01 18:25:00 +0600

    section-22

--- assets/js/section-22.js
+++ assets/js/section-22.js
@@ -5,6 +5,11 @@
     $(".video-play-btn").magnificPopup({
       type: "video",
     });
+    var s = skrollr.init{
+      render: function ("data") {
+          
+      }
+    }
   });
   $(Window).load(function () {});
 })(jQuery);
