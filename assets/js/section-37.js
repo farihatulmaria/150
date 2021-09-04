6ef6fd86d40d93e378584a7e00a237e2476244f8
farihatulmaria <farihatulmaria@gmail.com>
2021-09-04 18:22:46 +0600

    section-35 , 36 and 37

---
+++ assets/js/section-37.js
@@ -0,0 +1,11 @@
+(function ($) {
+  "use strict";
+  $(document).ready(function () {
+    $(".product-slider").slick({
+      dots: true,
+      slidesToScroll: 1,
+      //   autoplay: true,
+      //   autoplaySpeed: 1000,
+    });
+  });
+})(jQuery);
