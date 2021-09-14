518fae4ecdbeb0d534ec76edc3b83ab405d16701
farihatulmaria <farihatulmaria@gmail.com>
2021-09-14 17:53:49 +0600

    update

--- assets/js/section-12.js
+++ assets/js/section-12.js
@@ -7,5 +7,13 @@
       autoplay: true,
       autoplaySpeed: 2000,
     });
+    $(".menu-trigger").on("click", function () {
+      $(".offcanvas-menu").addClass("show-menu");
+      $(".offcanvas-shade").addClass("shade-active");
+    });
+    $(".menu-close").on("click", function () {
+      $(".offcanvas-menu").removeClass("show-menu");
+      $(".offcanvas-shade").removeClass("shade-active");
+    });
   });
 })(jQuery);
