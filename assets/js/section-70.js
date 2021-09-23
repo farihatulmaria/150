090e1d39bb8c16ebc3b660e7fa9d3e6873da812d
farihatulmaria <farihatulmaria@gmail.com>
2021-09-23 16:13:51 +0600

    update

---
+++ assets/js/section-70.js
@@ -0,0 +1,9 @@
+(function ($) {
+  "use strict";
+  jQuery(document).ready(function ($) {
+    $('#web-progress-bar').circleProgress({
+        value: 0.9,
+        size: 200,
+        fill: #01ccffd3,
+  });
+})(jQuery);
