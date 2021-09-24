0576c94638e7f9d5579c6c874d44af2eca572d37
farihatulmaria <farihatulmaria@gmail.com>
2021-09-24 16:04:32 +0600

    update

--- assets/js/section-70.js
+++ assets/js/section-70.js
@@ -1,9 +1,11 @@
 (function ($) {
   "use strict";
-  jQuery(document).ready(function ($) {
-    $('#web-progress-bar').circleProgress({
-        value: 0.9,
-        size: 200,
-        fill: #01ccffd3,
+  $(document).ready(function ($) {
+  $('#circle').circleProgress({
+    value: 0.75,
+    size: 80,
+    fill: {
+      gradient: ["red", "orange"]
+    }
   });
 })(jQuery);
--- assets/js/section-74.js
+++ assets/js/section-74.js
@@ -1,19 +1,9 @@
 (function ($) {
     "use strict";
     $(document).ready(function () {
-        $('.slider-for').slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows: false,
-            fade: true,
-            asNavFor: '.slider-nav'
-          });
-          $('.slider-nav').slick({
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            asNavFor: '.slider-for',
-            dots: true,
-            centerMode: true,
-            focusOnSelect: true
-          });
+      $('.sliders').slick({
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      });
   })(jQuery);
