414482b9e80652365275df859ce7635719233438
farihatulmaria <farihatulmaria@gmail.com>
2021-10-16 15:11:47 +0600

    update

--- assets/js/section-23.js
+++ assets/js/section-23.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-  //Check to see if the window is top if not then display button
   $(window).scroll(function () {
     if ($(this).scrollTop() > 100) {
       $(".scrollToTop").fadeIn();
@@ -8,7 +7,6 @@ $(document).ready(function () {
     }
   });
 
-  //Click event to scroll to top
   $(".scrollToTop").click(function () {
     $("html, body").animate({ scrollTop: 0 }, 800);
     return false;
